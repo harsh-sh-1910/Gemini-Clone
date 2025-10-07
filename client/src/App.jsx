8ae77a1fef42bc73771163cd17cd60b7a35b7adc
harsh-sh-1910 <harshsh1910@gmail.com>
2025-10-07 14:18:46 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import Sidebar from "./components/Sidebar/Sidebar";
+import Navbar from "./components/Navbar/Navbar";
+import Main from "./components/Main/Main";
+import Solutions from "./components/Solutions/Solutions";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Input from "./components/Input/Input";
+import UseContextProvider from "./UseContext/UsecontextProvider";
+
+const App = () => {
+  return (
+    <UseContextProvider>
+      <Router>
+        <div className="flex overflow-y-hidden">
+          <Sidebar />
+          <div className="w-full">
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<Main />} />
+              <Route path="/solutions" element={<Solutions />} />
+            </Routes>
+            <Input />
+          </div>
+        </div>
+      </Router>
+    </UseContextProvider>
+  );
+};
+
+export default App;
